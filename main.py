818bc8eada584b0b27460615993d0262c7e078af
smithbwlhs <smithb@wlwv.k12.or.us>
2024-09-20 09:18:47 -0700

    initial

---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+Name: Mr. Smith
+Date: 9-16-24
+Assignment: Unit 2 Lesson 1 Notes
+
+"""
+
+# Variables
+
+message = "Hello, user"
+print(message)
+
+# snake_case
+user_name = input("Enter your name: ")
+user_id = int(input("Enter your id: "))
+
+# variable type
+# print(type(user_name))
+
+# Strings
+# can use ' or " to indicate string - be consistent
+
+# f-strings are formatted strings that help with combining string
+# Way 1 to combine string: use +
+message_one = "Welcome " + user_name + " with ID " + str(user_id)
+print(message_one)
+
